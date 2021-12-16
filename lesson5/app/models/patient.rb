90fb2a6ea97f6db5896aa516b644f92478e1b3ec
Arsenii Iurkov <62850269+Alice932@users.noreply.github.com>
2021-12-16 17:20:27 +0200

    Update ration.rb

--- lesson5/app/models/patient.rb
+++ lesson5/app/models/patient.rb
@@ -4,7 +4,6 @@ class Patient < ApplicationRecord
     has_many :physicians, through: :appointments
 
     validates :first_name, :last_name, length: { in: 6..20 }
-    #validates :first_name, :last_name,
     validates :email, presence: true
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
     validates :age, presence: true, numericality: {only_integer: true, greater_than: 0, less_than: 100}
