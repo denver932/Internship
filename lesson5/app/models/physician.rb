fb78035243c9da7b5552eac88658af146afe2075
Alice932 <denver9321@gmail.com>
2022-01-03 14:55:38 +0200

    Add simple forms

--- lesson5/app/models/physician.rb
+++ lesson5/app/models/physician.rb
@@ -5,5 +5,5 @@ class Physician < ApplicationRecord
 
   validates :first_name, :last_name, length: { in: 6..20 }
   validates :phone, presence: true, format: { with: /\A\+?380\d{9}\z/, message: 'only UA phones' }
-  validates :level, presence: true, inclusion: { in: '%w(junior middle senior)' }
+  validates :level, presence: true, inclusion: { in: %w(junior middle senior) }
 end
