eb82533f5d5f397fc5a9ecb4e99d867b2851b2ff
Alice932 <denver9321@gmail.com>
2022-01-17 07:11:57 +0300

    Add ajax effects and add loading from json

--- lesson5/app/javascript/packs/load_diagnosis_items.js
+++ lesson5/app/javascript/packs/load_diagnosis_items.js
@@ -1,1 +1,16 @@
+function load() {
+  $(".show").click( function() {
+    $.ajax({
+      url: `/diagnosis_items.json?patient_id=${$(this).attr('id')}`,
+      dataType: "json",
+      success: function(data) {
+        const result = data.map((item) => item.content)
+        $(".load").text(result);
+      }
+    })
+  });
+}
 
+$(document).on('turbolinks:load', function () {
+  load();
+})
