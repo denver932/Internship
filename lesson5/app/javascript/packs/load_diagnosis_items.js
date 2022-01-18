c13045d58fef54c43b87c0120495627a49a1b22e
Alice932 <denver9321@gmail.com>
2022-01-18 18:38:11 +0300

    Edit load file

--- lesson5/app/javascript/packs/load_diagnosis_items.js
+++ lesson5/app/javascript/packs/load_diagnosis_items.js
@@ -1,12 +1,12 @@
 function load() {
-  $(".ssss").click( function() {
+  $(".show").click( function() {
     $.ajax({
-      url: `/diagnosis_items.json?patient_id=${$(this).attr('id')}`,
+      url: `/diagnosis_items.json?patient_id=${patientId}`,
       dataType: "json",
       method: "GET",
       success: function(data) {
         const result = data.map((item) => item.content)
-        $(".load").text(result);
+        $(`#show-items${patientId}`).html(result);
       }
     })
   });
